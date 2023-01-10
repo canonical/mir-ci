11c5572dd0b7af4d88e028a38c28edc732aade1a
Sophie Winter <wm@wmww.sh>
2023-01-10 09:39:29 -0800

    Use inotify library

--- frame/frame_test_case.py
+++ frame/frame_test_case.py
@@ -1,4 +1,5 @@
 from unittest import TestCase
+import inotify.adapters
 import subprocess
 import os
 import time
@@ -43,13 +44,33 @@ class Program:
             self.killed = True
             self.wait()
 
+def wait_for_wayland_display(name: str) -> None:
+    runtime_dir = os.environ['XDG_RUNTIME_DIR']
+    # Clear out any existing display before waiting for the new one
+    for path in [
+        os.path.join(runtime_dir, name),
+        os.path.join(runtime_dir, name + '.lock')
+    ]:
+        if os.path.exists(path):
+            os.remove(path)
+    # Set up inotify
+    i = inotify.adapters.Inotify()
+    i.add_watch(runtime_dir, mask=inotify.constants.IN_CREATE)
+    # Wait for display to appear
+    for event in i.event_gen(timeout_s=long_timeout, yield_nones=False):
+        (_, type_names, path, filename) = event
+        if filename == name:
+            return
+    # Raise timeout error if we didn't return
+    raise RuntimeError('Wayland display ' + name + ' did not appear')
+
 class FrameTestCase(TestCase):
     def setUp(self) -> None:
-        os.environ['WAYLAND_DISPLAY'] = 'wayland-99'
+        wayland_display = 'wayland-99'
+        os.environ['WAYLAND_DISPLAY'] = wayland_display
         self.frame = Program('ubuntu-frame')
         try:
-            inotify = Program('inotifywait', ['--event', 'create', os.environ['XDG_RUNTIME_DIR']])
-            inotify.wait()
+            wait_for_wayland_display(wayland_display)
         except:
             self.frame.kill()
             raise
