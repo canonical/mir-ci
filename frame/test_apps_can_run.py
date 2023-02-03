49434a4aac8db172de2a9ad0be0d00bd2f5e050b
Sophie Winter <wm@wmww.sh>
2023-02-03 17:28:20 +0000

    Paramaterize tests

--- frame/test_apps_can_run.py
+++ frame/test_apps_can_run.py
@@ -1,28 +1,33 @@
-from frame_test_case import FrameTestCase
+from frame_test_case import DisplayServer
+from parameterized import parameterized_class
+from unittest import TestCase
 import time
 
-short_wait_time = 5
+short_wait_time = 3
 
-class TestAppsCanRun(FrameTestCase):
-    def run_test(self, command: str) -> None:
-        app = self.program(command)
-        time.sleep(short_wait_time)
-        app.assert_running()
-
-    def test_wpe_webkit(self) -> None:
-        self.run_test('wpe-webkit-mir-kiosk.cog')
-
-    def test_neverputt(self) -> None:
-        self.run_test('mir-kiosk-neverputt')
-
-    def test_scummvm(self) -> None:
-        self.run_test('mir-kiosk-scummvm')
-
-    def test_kodi(self) -> None:
-        self.run_test('mir-kiosk-kodi')
-
-    def test_gedit(self) -> None:
-        self.run_test('gedit')
-
-    def test_qterminal(self) -> None:
-        self.run_test('qterminal')
+@parameterized_class([
+    {'server': 'ubuntu-frame', 'app': 'wpe-webkit-mir-kiosk.cog'},
+    {'server': 'ubuntu-frame', 'app': 'mir-kiosk-neverputt'},
+    {'server': 'ubuntu-frame', 'app': 'mir-kiosk-scummvm'},
+    {'server': 'ubuntu-frame', 'app': 'mir-kiosk-kodi'},
+    {'server': 'ubuntu-frame', 'app': 'gedit'},
+    {'server': 'ubuntu-frame', 'app': 'qterminal'},
+    {'server': 'mir-kiosk', 'app': 'wpe-webkit-mir-kiosk.cog'},
+    {'server': 'mir-kiosk', 'app': 'mir-kiosk-neverputt'},
+    {'server': 'mir-kiosk', 'app': 'mir-kiosk-scummvm'},
+    {'server': 'mir-kiosk', 'app': 'mir-kiosk-kodi'},
+    {'server': 'mir-kiosk', 'app': 'gedit'},
+    {'server': 'mir-kiosk', 'app': 'qterminal'},
+    {'server': 'egmde', 'app': 'wpe-webkit-mir-kiosk.cog'},
+    {'server': 'egmde', 'app': 'mir-kiosk-neverputt'},
+    {'server': 'egmde', 'app': 'mir-kiosk-scummvm'},
+    {'server': 'egmde', 'app': 'mir-kiosk-kodi'},
+    {'server': 'egmde', 'app': 'gedit'},
+    {'server': 'egmde', 'app': 'qterminal'},
+])
+class TestAppsCanRun(TestCase):
+    def test_app_can_run(self) -> None:
+        with DisplayServer(self.server) as server:
+            app = server.program(self.app)
+            time.sleep(short_wait_time)
+            app.assert_running()
