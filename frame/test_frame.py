a306ee4d03168c6ccac43c3db046690bfaff87f4
Sophie Winter <wm@wmww.sh>
2023-02-03 14:34:29 +0000

    Add more smoke tests

--- frame/test_frame.py
+++ frame/test_frame.py
@@ -1,18 +1,6 @@
 from frame_test_case import FrameTestCase
 import time
 
-short_wait_time = 5
-
 class FrameSmokeTests(FrameTestCase):
     def test_frame_can_run(self) -> None:
         pass
-
-    def test_wpe_webkit_can_run(self) -> None:
-        app = self.program('wpe-webkit-mir-kiosk.cog')
-        time.sleep(short_wait_time)
-        app.assert_running()
-
-    def test_neverputt_can_run(self) -> None:
-        app = self.program('mir-kiosk-neverputt')
-        time.sleep(short_wait_time)
-        app.assert_running()
