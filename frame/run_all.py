fb31336f9281986c68dbcea092b4120c471ee1ca
Sophie Winter <wm@wmww.sh>
2023-02-03 13:10:59 +0000

    Drop run_all.py

--- frame/run_all.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import unittest
-suite = unittest.TestSuite()
-suite.addTest(unittest.TestLoader().discover('.'))
-runner = unittest.TextTestRunner()
-result = runner.run(suite)
-exit(0 if result.wasSuccessful() else 1)
