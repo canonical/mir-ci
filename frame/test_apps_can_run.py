d24f9d8b3bf98d4b649ca7000fc66f235e5a3118
Sophie Winter <wm@wmww.sh>
2023-02-09 14:16:31 -0500

    Support automatic parameter combinations

--- frame/test_apps_can_run.py
+++ frame/test_apps_can_run.py
@@ -1,31 +1,20 @@
 from frame_test_case import DisplayServer
 from parameterized import parameterized
 from unittest import TestCase
+from helpers import combine, all_servers
 import time
 
 short_wait_time = 3
 
 class TestAppsCanRun(TestCase):
-    @parameterized.expand([
-        ('ubuntu-frame', 'wpe-webkit-mir-kiosk.cog'),
-        ('ubuntu-frame', 'mir-kiosk-neverputt'),
-        ('ubuntu-frame', 'mir-kiosk-scummvm'),
-        ('ubuntu-frame', 'mir-kiosk-kodi'),
-        ('ubuntu-frame', 'gedit'),
-        ('ubuntu-frame', 'qterminal'),
-        ('mir-kiosk', 'wpe-webkit-mir-kiosk.cog'),
-        ('mir-kiosk', 'mir-kiosk-neverputt'),
-        ('mir-kiosk', 'mir-kiosk-scummvm'),
-        ('mir-kiosk', 'mir-kiosk-kodi'),
-        ('mir-kiosk', 'gedit'),
-        ('mir-kiosk', 'qterminal'),
-        ('egmde', 'wpe-webkit-mir-kiosk.cog'),
-        ('egmde', 'mir-kiosk-neverputt'),
-        ('egmde', 'mir-kiosk-scummvm'),
-        ('egmde', 'mir-kiosk-kodi'),
-        ('egmde', 'gedit'),
-        ('egmde', 'qterminal'),
-    ])
+    @parameterized.expand(combine(all_servers(), [
+        'wpe-webkit-mir-kiosk.cog',
+        'mir-kiosk-neverputt',
+        'mir-kiosk-scummvm',
+        'mir-kiosk-kodi',
+        'gedit',
+        'qterminal',
+    ]))
     def test_app_can_run(self, server, app) -> None:
         with DisplayServer(server) as server:
             app = server.program(app)
