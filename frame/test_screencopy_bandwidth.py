58dddd40ba54845602a5c05ffa27abeb4514b5ba
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-03-22 19:54:56 +0100

    bwidth: add a QTerminal fixture ensuring clean runs
    QTerminal will remember a lot about itself between runs, and also
    depending on version won't exit when the command given it exits.

--- frame/test_screencopy_bandwidth.py
+++ frame/test_screencopy_bandwidth.py
@@ -2,22 +2,34 @@ from display_server import DisplayServer
 from parameterized import parameterized
 from unittest import TestCase
 from helpers import all_servers
+from program import AppFixture
 import itertools
-import tempfile
 import os
 
 
 ASCIINEMA_CAST = f'{os.path.dirname(__file__)}/data/demo.cast'
 
+
+class QTerminal(AppFixture):
+    executable = 'qterminal'
+
+    def env(self) -> dict[str, str]:
+        env = super().env()
+        config = self.temppath / 'qterminal.org' / 'qterminal.ini'
+        config.parent.mkdir(parents=True)
+        with open(config, 'w') as c:
+            c.write('[General]\nAskOnExit=false')
+
+        return env | {
+            'XDG_CONFIG_HOME': str(self.temppath)
+        }
+
+
 class TestScreencopyBandwidth(TestCase):
     @parameterized.expand(itertools.product(all_servers(), [
-        (('qterminal', '--execute', f'asciinema play {ASCIINEMA_CAST}'), 30),
+        (QTerminal('--execute', f'asciinema play {ASCIINEMA_CAST}'), 15),
     ]))
     def test_app_can_run(self, server, app) -> None:
-        with DisplayServer(server) as s, tempfile.TemporaryDirectory() as d:
-            env = {
-                'XDG_DATA_HOME': f"{d}/data",
-                'XDG_CONFIG_HOME': f"{d}/config",
-            }
-            with s.program(app[0], env) as a:
-                a.wait(app[1])
+        with DisplayServer(server) as s, app[0] as a:
+            with s.program(a) as p:
+                p.wait(app[1])
