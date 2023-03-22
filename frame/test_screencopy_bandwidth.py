29223616ae45913871ba9549a5c6f63ae75d9fb8
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-03-22 18:18:51 +0100

    bandwidth: use temporary XDG dirs
    This way things don't remember e.g. window locations between runs,
    which would impact test results.

--- frame/test_screencopy_bandwidth.py
+++ frame/test_screencopy_bandwidth.py
@@ -3,6 +3,7 @@ from parameterized import parameterized
 from unittest import TestCase
 from helpers import all_servers
 import itertools
+import tempfile
 import os
 
 
@@ -13,6 +14,10 @@ class TestScreencopyBandwidth(TestCase):
         (('qterminal', '--execute', f'asciinema play {ASCIINEMA_CAST}'), 30),
     ]))
     def test_app_can_run(self, server, app) -> None:
-        with DisplayServer(server) as s:
-            with s.program(app[0]) as a:
+        with DisplayServer(server) as s, tempfile.TemporaryDirectory() as d:
+            env = {
+                'XDG_DATA_HOME': f"{d}/data",
+                'XDG_CONFIG_HOME': f"{d}/config",
+            }
+            with s.program(app[0], env) as a:
                 a.wait(app[1])
