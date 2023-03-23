7ab8cdbab6b743b68c44da1ad50d2f22c6c1508b
Sophie Winter <git@phie.me>
2023-03-23 15:05:41 -0700

    TestScreencopyBandwidth: record properties

--- frame/test_screencopy_bandwidth.py
+++ frame/test_screencopy_bandwidth.py
@@ -1,4 +1,5 @@
 from display_server import DisplayServer
+from screencopy_tracker import ScreencopyTracker
 from helpers import all_servers
 from program import AppFixture
 import pytest
@@ -29,18 +30,26 @@ class TestScreencopyBandwidth:
         (QTerminal('--execute', f'asciinema play {ASCIINEMA_CAST}'), 15),
         (AppFixture("mir-kiosk-neverputt"), None),
     ])
-    def test_active_app(self, server, app) -> None:
+    def test_active_app(self, record_property, server, app) -> None:
         with DisplayServer(server) as s, app[0] as a:
-            with s.program(a) as p:
-                if app[1] is not None:
-                    p.wait(app[1])
-                else:
-                    time.sleep(long_wait_time)
+            tracker = ScreencopyTracker(s.display_name)
+            with tracker:
+                with s.program(a) as p:
+                    if app[1] is not None:
+                        p.wait(app[1])
+                    else:
+                        time.sleep(long_wait_time)
+            for name, val in tracker.properties().items():
+                record_property(name, val)
 
     @pytest.mark.parametrize('server', all_servers())
-    def test_compositor_alone(self, server) -> None:
+    def test_compositor_alone(self, record_property, server) -> None:
         with DisplayServer(server) as s:
-            time.sleep(long_wait_time)
+            tracker = ScreencopyTracker(s.display_name)
+            with tracker:
+                time.sleep(long_wait_time)
+            for name, val in tracker.properties().items():
+                record_property(name, val)
 
     @pytest.mark.parametrize('server', all_servers())
     @pytest.mark.parametrize('app', [
@@ -48,7 +57,11 @@ class TestScreencopyBandwidth:
         ("gedit", "-s"),
         "mir-kiosk-kodi",
     ])
-    def test_inactive_app(self, server, app) -> None:
+    def test_inactive_app(self, record_property, server, app) -> None:
         with DisplayServer(server) as s:
-            with s.program(app):
-                time.sleep(long_wait_time)
+            tracker = ScreencopyTracker(s.display_name)
+            with tracker:
+                with s.program(app):
+                    time.sleep(long_wait_time)
+            for name, val in tracker.properties().items():
+                record_property(name, val)
