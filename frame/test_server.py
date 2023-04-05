b849365b1aa456c36b9b52ad51bd7f559ce37edd
Sophie Winter <git@phie.me>
2023-04-05 11:09:35 -0700

    Fix name of smoke tests class

--- frame/test_server.py
+++ frame/test_server.py
@@ -2,7 +2,7 @@ from helpers import all_servers
 from display_server import DisplayServer
 import pytest
 
-class ServerSmokeTests:
+class TestServerCanRun:
     @pytest.mark.parametrize('server', all_servers())
     def test_server_can_run(self, server) -> None:
         with DisplayServer(server) as server:
