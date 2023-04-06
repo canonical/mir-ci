cdbde0f0e662624f208292290ba04779ab39b372
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-04-06 15:16:18 +0200

    test: move mypy to a fixture

--- frame/test_tests.py
+++ frame/test_tests.py
@@ -1,17 +1,12 @@
-import subprocess
 import os
 
 import pytest
 
 class TestTest:
     @pytest.mark.self
-    def test_project_typechecks(self) -> None:
+    def test_project_typechecks(self, mypy) -> None:
         project_path = os.path.dirname(__file__)
         assert os.path.isfile(os.path.join(project_path, 'requirements.txt')), 'project path not detected correctly'
-        result = subprocess.run(
-            ['mypy', project_path],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True)
-        if result.returncode != 0:
-            raise RuntimeError('`$ mypy ' + project_path + '` failed:\n\n' + result.stdout)
+        result = mypy.api.run([project_path])
+        if result[2] != 0:
+            raise RuntimeError('`$ mypy ' + project_path + '` failed:\n\n' + result[0])
