dc184b27452eab6960ce1c03eccdb563f4f164d7
Sophie Winter <git@phie.me>
2023-04-06 18:58:58 -0700

    Update test_screencopy_bandwidth.py to use asyncio

--- frame/test_screencopy_bandwidth.py
+++ frame/test_screencopy_bandwidth.py
@@ -2,7 +2,7 @@ from display_server import DisplayServer
 from screencopy_tracker import ScreencopyTracker
 import pytest
 import os
-import time
+import asyncio
 
 import apps
 
@@ -16,22 +16,22 @@ class TestScreencopyBandwidth:
         apps.qterminal('--execute', f'python3 -m asciinema play {ASCIINEMA_CAST}', pip_pkgs=('asciinema',), id='asciinema', extra=15),
         apps.snap('mir-kiosk-neverputt', extra=False)
     ])
-    def test_active_app(self, record_property, server, app) -> None:
+    async def test_active_app(self, record_property, server, app) -> None:
         with DisplayServer(server, add_extensions=ScreencopyTracker.required_extensions) as s:
             tracker = ScreencopyTracker(s.display_name)
             with tracker, s.program(app[0]) as p:
                 if app[1]:
                     p.wait(app[1])
                 else:
-                    time.sleep(long_wait_time)
+                    await asyncio.sleep(long_wait_time)
             for name, val in tracker.properties().items():
                 record_property(name, val)
 
-    def test_compositor_alone(self, record_property, server) -> None:
+    async def test_compositor_alone(self, record_property, server) -> None:
         with DisplayServer(server, add_extensions=ScreencopyTracker.required_extensions) as s:
             tracker = ScreencopyTracker(s.display_name)
             with tracker:
-                time.sleep(long_wait_time)
+                await asyncio.sleep(long_wait_time)
             for name, val in tracker.properties().items():
                 record_property(name, val)
 
@@ -40,10 +40,10 @@ class TestScreencopyBandwidth:
         apps.gedit(),
         apps.snap('mir-kiosk-kodi'),
     ])
-    def test_inactive_app(self, record_property, server, app) -> None:
+    async def test_inactive_app(self, record_property, server, app) -> None:
         with DisplayServer(server, add_extensions=ScreencopyTracker.required_extensions) as s:
             tracker = ScreencopyTracker(s.display_name)
             with tracker, s.program(app):
-                time.sleep(long_wait_time)
+                await asyncio.sleep(long_wait_time)
             for name, val in tracker.properties().items():
                 record_property(name, val)
