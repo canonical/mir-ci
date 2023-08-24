072e5d938a623ca9c01a1b1cad8995f7a51d8b4c
Alan Griffiths <alan@octopull.co.uk>
2023-08-24 15:22:05 +0100

    More tests

--- frame/test_drag_and_drop.py
+++ frame/test_drag_and_drop.py
@@ -12,6 +12,8 @@ except ModuleNotFoundError as e:
     pass
 
 APP_PATH = Path(__file__).parent / 'clients' / 'drag_and_drop_demo.py'
+STARTUP_TIME=1.5
+A_SHORT_TIME=0.3
 
 @pytest.mark.parametrize('modern_server', [
     apps.ubuntu_frame(),
@@ -33,16 +35,41 @@ class TestDragAndDrop:
         program = modern_server.program(app)
 
         async with modern_server, program, pointer:
-            await asyncio.sleep(1)
+            await asyncio.sleep(STARTUP_TIME)
             pointer.move_to_absolute(40, 40)
-            await asyncio.sleep(1)
+            await asyncio.sleep(A_SHORT_TIME)
             pointer.button(Button.LEFT, True)
-            await asyncio.sleep(1)
+            await asyncio.sleep(A_SHORT_TIME)
             pointer.move_to_absolute(120, 70)
-            await asyncio.sleep(1)
+            await asyncio.sleep(A_SHORT_TIME)
             pointer.move_to_absolute(200, 100)
-            await asyncio.sleep(1)
+            await asyncio.sleep(A_SHORT_TIME)
             pointer.button(Button.LEFT, False)
             await program.wait()
 
-        assert program.process.returncode == 0
+    @pytest.mark.parametrize('app', [
+        ('python3', str(APP_PATH), '--source', 'pixbuf', '--target', 'text', '--expect', 'pixbuf'),
+        ('python3', str(APP_PATH), '--source', 'text', '--target', 'pixbuf', '--expect', 'text'),
+        ('python3', str(APP_PATH), '--source', 'pixbuf', '--target', 'text', '--expect', 'text'),
+        ('python3', str(APP_PATH), '--source', 'text', '--target', 'pixbuf', '--expect', 'pixbuf'),
+    ])
+    @pytest.mark.deps(debs=('libgtk-4-dev',), pip_pkgs=(('pygobject', 'gi'),))
+    async def test_source_and_dest_mismatch(self, modern_server, app) -> None:
+        modern_server = DisplayServer(modern_server, add_extensions=VirtualPointer.required_extensions)
+        pointer = VirtualPointer(modern_server.display_name)
+        program = modern_server.program(app)
+
+        async with modern_server, program, pointer:
+            await asyncio.sleep(STARTUP_TIME)
+            pointer.move_to_absolute(40, 40)
+            await asyncio.sleep(A_SHORT_TIME)
+            pointer.button(Button.LEFT, True)
+            await asyncio.sleep(A_SHORT_TIME)
+            pointer.move_to_absolute(120, 70)
+            await asyncio.sleep(A_SHORT_TIME)
+            pointer.move_to_absolute(200, 100)
+            await asyncio.sleep(A_SHORT_TIME)
+            pointer.button(Button.LEFT, False)
+            await asyncio.sleep(A_SHORT_TIME)
+            assert program.is_running()
+            await program.kill()
