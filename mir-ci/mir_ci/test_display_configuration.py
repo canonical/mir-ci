8d7fde7bf0fbe206ac324fd44ef41cb09b067ae1
Matthew Kosarek <matthew@matthewkosarek.xyz>
2023-10-24 11:15:45 -0400

    Testing if can also change the position

--- mir-ci/mir_ci/test_display_configuration.py
+++ mir-ci/mir_ci/test_display_configuration.py
@@ -8,40 +8,64 @@ import pytest
 from mir_ci import SLOWDOWN, apps
 from mir_ci.display_server import DisplayServer
 from mir_ci.output_watcher import OutputWatcher
+from typing import Optional, Any
 
 short_wait_time = 1 * SLOWDOWN
 
 
-class TestDisplayConfiguration:
-    @pytest.mark.parametrize(
-        "local_server",
-        [
-            # TODO: Test other servers. Frame-based servers use
-            # a configuration file that is within the snap, so it
-            # is unclear how to read/modify that file from the outside
-            apps.mir_demo_server(),
-        ],
-    )
-    @pytest.mark.deps(pip_pkgs=("pyyaml",))
-    async def test_can_update_scale(self, local_server) -> None:
+class DisplayServerStaticFile:
+    def __init__(self, local_server):
+        self.local_server = local_server
         tmp_file = tempfile.NamedTemporaryFile(delete=False)
-        tmp_filename = tmp_file.name
+        self.tmp_filename = tmp_file.name
         try:
-            os.remove(tmp_filename)
+            os.remove(self.tmp_filename)
         except OSError:
             pass
 
-        server = DisplayServer(local_server, env={"MIR_SERVER_DISPLAY_CONFIG": f"static={tmp_filename}"})
+        self.server = DisplayServer(
+            self.local_server,
+            env={"MIR_SERVER_DISPLAY_CONFIG": f"static={self.tmp_filename}"})
+
+    async def __aenter__(self) -> "DisplayServer":
+        await self.server.__aenter__()
+
+    async def __aexit__(self, *args):
+        await self.server.__aexit__(*args)
+
+    def read_config(self) -> Any:
+        yaml = importlib.import_module("yaml")
+        with open(self.tmp_filename, "r") as file:
+            # the yaml parser doesn't like tabs before our comments
+            content = file.read().replace("\t#", "  #")
+            data = yaml.safe_load(content)
+            return data
+
+    def write_config(self, content: Any) -> None:
+        yaml = importlib.import_module("yaml")
+        with open(self.tmp_filename, "w") as file:
+            yaml.dump(content, file)
+
+
+@pytest.mark.parametrize(
+    "local_server",
+    [
+        # TODO: Test other servers. Frame-based servers use
+        # a configuration file that is within the snap, so it
+        # is unclear how to read/modify that file from the outside
+        apps.mir_demo_server(),
+    ],
+)
+@pytest.mark.deps(pip_pkgs=("pyyaml",))
+class TestDisplayConfiguration:
+    async def test_can_update_scale(self, local_server) -> None:
+        server = DisplayServerStaticFile(local_server)
         on_scale = Mock()
-        watcher = OutputWatcher(server.display_name, on_scale=on_scale)
+        watcher = OutputWatcher(server.server.display_name, on_scale=on_scale)
 
         async with server, watcher:
-            yaml = importlib.import_module("yaml")
             await asyncio.sleep(short_wait_time)
-            with open(tmp_filename, "r") as file:
-                # the yaml parser doesn't like tabs before our comments
-                content = file.read().replace("\t#", "  #")
-                data = yaml.safe_load(content)
+            data = server.read_config()
 
             card = data["layouts"]["default"]["cards"][0]
             for v in card.values():
@@ -49,8 +73,28 @@ class TestDisplayConfiguration:
                     v["scale"] = 2
                     break
 
-            with open(tmp_filename, "w") as file:
-                yaml.dump(data, file)
+            server.write_config(data)
             await asyncio.sleep(short_wait_time)
 
         on_scale.assert_called_with(ANY, 2)
+
+    async def test_can_update_position(self, local_server) -> None:
+        server = DisplayServerStaticFile(local_server)
+        on_geometry = Mock()
+        watcher = OutputWatcher(server.server.display_name, on_geometry=on_geometry)
+
+        async with server, watcher:
+            await asyncio.sleep(short_wait_time)
+            data = server.read_config()
+
+            card = data["layouts"]["default"]["cards"][0]
+            for v in card.values():
+                if type(v) is dict:
+                    v["position"] = [10, 20]
+                    break
+
+            server.write_config(data)
+            await asyncio.sleep(short_wait_time)
+
+        on_geometry.assert_called_with(ANY, 10, 20, ANY, ANY, ANY, ANY, ANY, ANY)
+
