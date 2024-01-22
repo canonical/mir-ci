d899d02012ad3d3601c31556d7e1bf5072f23840
Harlen Batagelo <hbatagelo@gmail.com>
2024-01-22 10:39:36 -0300

    Use test template

--- mir-ci/mir_ci/robot/libraries/DragAndDrop.py
+++ mir-ci/mir_ci/robot/libraries/DragAndDrop.py
@@ -1,81 +1,87 @@
 import asyncio
 
-from robot.api.deco import not_keyword
+from robot.api.deco import keyword, library
 
 from mir_ci import apps
 from mir_ci.display_server import DisplayServer
+from mir_ci.program import Program
 from mir_ci.virtual_pointer import Button, VirtualPointer
 
+@library(scope='TEST')
 class DragAndDrop:
-    ROBOT_LIBRARY_SCOPE = 'TEST'
-
     def __init__(self) -> None:
         self._actions = []
-        self._command = None
+        self._app_command = None
         self._pointer = None
         self._program = None
         self._server = None
-        self._server_cmd = None
+        self._server_command = None
 
-    def run_async(self, server_name : str, app_command : str):
-        self._server_cmd = getattr(apps, server_name)().values[0]
-        self._command = tuple(app_command.split())
+    @keyword
+    def run_async(self, server_name : str, app_command : str, flush : bool = True) -> None:
+        self._server_command = getattr(apps, server_name)().values[0]
+        self._app_command = tuple(app_command.split())
 
         if self._actions:
             asyncio.run(self.start_async())
-            self._actions = []
+            if flush:
+                self._actions = []
 
+    @keyword
     def sleep_for(self, time : float):
         self.register_action(lambda _: asyncio.sleep(time), should_await = True)
 
+    @keyword
     def move_pointer_to_absolute(self, x : float, y : float):
-        self.register_action(lambda context: context.pointer.move_to_absolute(x, y))
+        self.register_action(lambda self: self.pointer.move_to_absolute(x, y))
 
+    @keyword
     def press_left_button(self):
-        self.register_action(lambda context: context.pointer.button(Button.LEFT, True))
+        self.register_action(lambda self: self.pointer.button(Button.LEFT, True))
 
+    @keyword
     def release_left_button(self):
-        self.register_action(lambda context: context.pointer.button(Button.LEFT, False))
+        self.register_action(lambda self: self.pointer.button(Button.LEFT, False))
 
+    @keyword
     def wait_program(self):
-        self.register_action(lambda context: context.program.wait(), should_await = True)
+        self.register_action(lambda self: self.program.wait(), should_await = True)
 
+    @keyword
     def kill_program(self):
-        self.register_action(lambda context: context.program.kill(), should_await = True)
+        self.register_action(lambda self: self.program.kill(), should_await = True)
 
+    @keyword
     def assert_program_is_running(self):
-        self.register_action(lambda context: DragAndDrop.assert_helper(context.program.is_running(),
-                                                                       "Program should be running!"))
-
+        self.register_action(lambda self: self.assert_helper(self.program.is_running(),
+                                                             "Program should be running!"))
+    @keyword
     def program_output(self):
         return self.program.output
 
     @property
-    def pointer(self):
+    def pointer(self) -> VirtualPointer:
         if not self._pointer:
             raise SystemError('Pointer not found!')
         return self._pointer
 
     @property
-    def program(self):
+    def program(self) -> Program:
         if not self._program:
             raise SystemError('Program not found!')
         return self._program
 
     @staticmethod
-    @not_keyword
     def assert_helper(condition, error_message : str = ''):
-        assert condition, 'Assertion failed! ' + error_message
+        assert condition, f'Assertion failed! {error_message}'
 
-    @not_keyword
     def register_action(self, action, should_await = False):
         self._actions.append((action, should_await))
 
-    @not_keyword
     async def start_async(self):
-        self._server = DisplayServer(self._server_cmd, add_extensions=VirtualPointer.required_extensions)
+        self._server = DisplayServer(self._server_command, add_extensions=VirtualPointer.required_extensions)
         self._pointer = VirtualPointer(self._server.display_name)
-        self._program = self._server.program(apps.App(self._command))
+        self._program = self._server.program(apps.App(self._app_command))
 
         async with self._server, self._program, self._pointer:
             for action, should_await in self._actions:
