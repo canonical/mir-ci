70fb22b7caef05c95eb3dbda372a22a039d00452
Harlen Batagelo <hbatagelo@gmail.com>
2024-01-23 16:51:44 -0300

    Add automatic display connection/disconnection

--- mir-ci/mir_ci/robot/libraries/WaylandHid.py
+++ mir-ci/mir_ci/robot/libraries/WaylandHid.py
@@ -1,30 +1,42 @@
+import asyncio
+
 from robot.api.deco import keyword, library
 from mir_ci.virtual_pointer import VirtualPointer, Button
 
 
-@library(scope='TEST')
+@library(scope='TEST', listener='SELF')
 class WaylandHid(VirtualPointer):
 
-    @keyword
-    def move_pointer_to_absolute(self, x: float, y: float) -> None:
+    def __init__(self, display_name: str) -> None:
+        super().__init__(display_name)
+        self.event_loop = None
+
+    @keyword()
+    async def move_pointer_to_absolute(self, x: float, y: float) -> None:
+        await self.connect()
         self.move_to_absolute(x, y)
 
     @keyword
-    def move_pointer_to_proportional(self, x: float, y: float) -> None:
+    async def move_pointer_to_proportional(self, x: float, y: float) -> None:
+        await self.connect()
         self.move_to_proportional(x, y)
 
     @keyword("Press ${button} Button")
-    def press_pointer_button(self, button: str) -> None:
+    async def press_pointer_button(self, button: str) -> None:
+        await self.connect()
         self.button(Button[button], True)
 
     @keyword("Release ${button} Button")
-    def release_pointer_button(self, button: str) -> None:
+    async def release_pointer_button(self, button: str) -> None:
+        await self.connect()
         self.button(Button[button], False)
 
-    @keyword
     async def connect(self):
+        if self.pointer:
+            return
         await self.__aenter__()  # pylint: disable=unnecessary-dunder-call
+        self.event_loop = asyncio.get_event_loop()
 
-    @keyword
-    async def disconnect(self):
-        await self.__aexit__()
+    def _end_test(self, data, result):
+        _ = (data, result)
+        self.event_loop.run_until_complete(self.__aexit__())
