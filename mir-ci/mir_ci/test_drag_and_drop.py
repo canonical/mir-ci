813b287bae5cfdf0265065abfe4dfdff8211fc08
Harlen Batagelo <hbatagelo@gmail.com>
2024-01-24 17:41:07 -0300

    More cleanup

--- mir-ci/mir_ci/test_drag_and_drop.py
+++ mir-ci/mir_ci/test_drag_and_drop.py
@@ -1,15 +1,18 @@
-import asyncio
+import tempfile
 from pathlib import Path
 
 import pytest
 from mir_ci import SLOWDOWN, apps
 from mir_ci.display_server import DisplayServer
-from mir_ci.virtual_pointer import Button, VirtualPointer
+from mir_ci.virtual_pointer import VirtualPointer
 
-APP_PATH = Path(__file__).parent / "clients" / "drag_and_drop_demo.py"
+MIR_CI_PATH = str(Path(__file__).parent)
+APP_PATH = MIR_CI_PATH + "/clients/drag_and_drop_demo.py"
 STARTUP_TIME = 1.5 * SLOWDOWN
 A_SHORT_TIME = 0.3
-
+ROBOT_HEADER = f"""*** Settings ***
+Library    {MIR_CI_PATH}/robot/libraries/WaylandHid.py
+"""
 
 @pytest.mark.parametrize(
     "modern_server",
@@ -25,58 +28,72 @@ class TestDragAndDrop:
     @pytest.mark.parametrize(
         "app",
         [
-            ("python3", str(APP_PATH), "--source", "pixbuf", "--target", "pixbuf", "--expect", "pixbuf"),
-            ("python3", str(APP_PATH), "--source", "text", "--target", "text", "--expect", "text"),
+            ("python3", APP_PATH, "--source", "pixbuf", "--target", "pixbuf", "--expect", "pixbuf"),
+            ("python3", APP_PATH, "--source", "text", "--target", "text", "--expect", "text"),
         ],
     )
     @pytest.mark.deps(debs=("libgtk-4-dev",), pip_pkgs=(("pygobject", "gi"),))
     async def test_source_and_dest_match(self, modern_server, app) -> None:
         modern_server = DisplayServer(modern_server, add_extensions=VirtualPointer.required_extensions)
-        pointer = VirtualPointer(modern_server.display_name)
         program = modern_server.program(apps.App(app))
 
-        async with modern_server, program, pointer:
-            await asyncio.sleep(STARTUP_TIME)
-            pointer.move_to_absolute(40, 40)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.button(Button.LEFT, True)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.move_to_absolute(120, 70)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.move_to_absolute(200, 100)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.button(Button.LEFT, False)
-            await program.wait()
+        robot_test_case = f"""Source and Destination Match
+            Sleep     {STARTUP_TIME}
+            Move Pointer To Absolute    40    40
+            Sleep     {A_SHORT_TIME}
+            Press LEFT Button
+            Sleep     {A_SHORT_TIME}
+            Move Pointer To Absolute    120    70
+            Sleep     {A_SHORT_TIME}
+            Move Pointer To Absolute    200    100
+            Sleep     {A_SHORT_TIME}
+            Release LEFT Button
+        """
+
+        with tempfile.NamedTemporaryFile(mode='w+', suffix='.robot', buffering=1) as robot_file:
+            robot_file.write(ROBOT_HEADER + "\n*** Test Cases ***\n" + robot_test_case)
+            robot = modern_server.program(apps.App(("robot", "-o", "NONE", "-r", "NONE", robot_file.name)))
+
+            async with modern_server, program, robot:
+                await robot.wait()
+                await program.wait()
 
     @pytest.mark.parametrize(
         "app",
         [
-            ("python3", "-u", str(APP_PATH), "--source", "pixbuf", "--target", "text", "--expect", "pixbuf"),
-            ("python3", "-u", str(APP_PATH), "--source", "text", "--target", "pixbuf", "--expect", "text"),
-            ("python3", "-u", str(APP_PATH), "--source", "pixbuf", "--target", "text", "--expect", "text"),
-            ("python3", "-u", str(APP_PATH), "--source", "text", "--target", "pixbuf", "--expect", "pixbuf"),
+            ("python3", "-u", APP_PATH, "--source", "pixbuf", "--target", "text", "--expect", "pixbuf"),
+            ("python3", "-u", APP_PATH, "--source", "text", "--target", "pixbuf", "--expect", "text"),
+            ("python3", "-u", APP_PATH, "--source", "pixbuf", "--target", "text", "--expect", "text"),
+            ("python3", "-u", APP_PATH, "--source", "text", "--target", "pixbuf", "--expect", "pixbuf"),
         ],
     )
     @pytest.mark.deps(debs=("libgtk-4-dev",), pip_pkgs=(("pygobject", "gi"),))
     async def test_source_and_dest_mismatch(self, modern_server, app) -> None:
         modern_server = DisplayServer(modern_server, add_extensions=VirtualPointer.required_extensions)
-        pointer = VirtualPointer(modern_server.display_name)
         program = modern_server.program(apps.App(app))
 
-        async with modern_server, program, pointer:
-            await asyncio.sleep(STARTUP_TIME)
-            pointer.move_to_absolute(40, 40)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.button(Button.LEFT, True)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.move_to_absolute(120, 70)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.move_to_absolute(200, 100)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.button(Button.LEFT, False)
-            await asyncio.sleep(A_SHORT_TIME)
-            pointer.move_to_absolute(220, 120)
-            await asyncio.sleep(A_SHORT_TIME)
-            assert program.is_running()
-            await program.kill()
-        assert "drag-begin\ndrag-failed\nenter-notify-event: dropbox" in program.output
+        robot_test_case = f"""Source and Destination Mismatch
+            Sleep     {STARTUP_TIME}
+            Move Pointer To Absolute    40    40
+            Sleep     {A_SHORT_TIME}
+            Press LEFT Button
+            Sleep     {A_SHORT_TIME}
+            Move Pointer To Absolute    120    70
+            Sleep     {A_SHORT_TIME}
+            Move Pointer To Absolute    200    100
+            Sleep     {A_SHORT_TIME}
+            Release LEFT Button
+            Sleep     {A_SHORT_TIME}
+            Move Pointer To Absolute    220    120
+            Sleep     {A_SHORT_TIME}
+        """
+
+        with tempfile.NamedTemporaryFile(mode='w+', suffix='.robot', buffering=1) as robot_file:
+            robot_file.write(ROBOT_HEADER + "\n*** Test Cases ***\n" + robot_test_case)
+            robot = modern_server.program(apps.App(("robot", "-o", "NONE", "-r", "NONE", robot_file.name)))
+
+            async with modern_server, program, robot:
+                await robot.wait()
+                assert program.is_running()
+                await program.kill()
+            assert "drag-begin\ndrag-failed\nenter-notify-event: dropbox" in program.output
