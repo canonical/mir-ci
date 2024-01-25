9bd09699332f02146f1473a808aa9b02c93bd7a9
Harlen Batagelo <hbatagelo@gmail.com>
2024-01-25 14:07:03 -0300

    Simplify the handling of WAYLAND_DISPLAY and make the code more clear

--- mir-ci/mir_ci/robot/libraries/WaylandHid.py
+++ mir-ci/mir_ci/robot/libraries/WaylandHid.py
@@ -3,7 +3,6 @@ import os
 
 from mir_ci.virtual_pointer import Button, VirtualPointer
 from robot.api.deco import keyword, library
-from robot.api.exceptions import FatalError
 
 
 @library(scope="TEST", listener="SELF")
@@ -14,17 +13,11 @@ class WaylandHid(VirtualPointer):
     The client connects to the display upon entering the first keyword,
     and disconnects when the test case ends.
 
-    A FatalError is raised if WAYLAND_DISPLAY is not defined.
-    If WAYLAND_DISPLAY is defined but empty, it defaults to 'wayland-0'.
+    If WAYLAND_DISPLAY is not defined, it defaults to 'wayland-0'.
     """
 
     def __init__(self) -> None:
-        display_name = os.environ.get("WAYLAND_DISPLAY")
-        if display_name is None:
-            raise FatalError("WAYLAND_DISPLAY is not defined.")
-        if not display_name:
-            display_name = "wayland-0"
-
+        display_name = os.environ.get("WAYLAND_DISPLAY", "wayland-0")
         super().__init__(display_name)
 
     @keyword
@@ -54,9 +47,13 @@ class WaylandHid(VirtualPointer):
     async def connect(self):
         """Connect to the display."""
         if not self.pointer:
-            await self.__aenter__()  # pylint: disable=unnecessary-dunder-call
+            await super().connect()
+
+    async def disconnect(self):
+        """Disconnect from the display."""
+        if self.pointer:
+            await super().disconnect()
 
     def _end_test(self, data, result):  # pylint: disable=unused-argument
-        """Listener method called at the end of the test case to disconnect the client."""
-        if self.pointer:
-            asyncio.get_event_loop().run_until_complete(self.__aexit__())
+        """Listener method called at the end of the test case."""
+        asyncio.get_event_loop().run_until_complete(self.disconnect())
