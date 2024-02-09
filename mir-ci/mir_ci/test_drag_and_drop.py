6df9bab1d2ed767ffc52bbd2def7e1fc002223d4
Michał Sawicz <michal@sawicz.net>
2024-02-09 19:55:23 +0100

    dnd: use WaylandRobot wrapper

--- mir-ci/mir_ci/test_drag_and_drop.py
+++ mir-ci/mir_ci/test_drag_and_drop.py
@@ -1,32 +1,14 @@
-import tempfile
 from pathlib import Path
 from textwrap import dedent
 
 import pytest
 from mir_ci import apps
 from mir_ci.display_server import DisplayServer
+from mir_ci.robot_wrapper import WaylandRobot
 from mir_ci.screencopy_tracker import ScreencopyTracker
 from mir_ci.virtual_pointer import VirtualPointer
 
-MIR_CI_PATH = Path(__file__).parent
-APP_PATH = MIR_CI_PATH / "clients/drag_and_drop_demo.py"
-
-ROBOT_TEMPLATE = """\
-*** Settings ***
-{settings}
-
-*** Variables ***
-{variables}
-
-*** Test Cases ***
-{test_case}
-"""
-ROBOT_SETTINGS = f"Resource   {MIR_CI_PATH}/robot_resources/screencopy.resource"
-ROBOT_VARIABLES = f"""\
-${{SRC_TEMPLATE}}    {MIR_CI_PATH}/robot_templates/drag_and_drop_src.png
-${{DST_TEMPLATE}}    {MIR_CI_PATH}/robot_templates/drag_and_drop_dst.png
-${{END_TEMPLATE}}    {MIR_CI_PATH}/robot_templates/drag_and_drop_end.png
-"""
+APP_PATH = Path(__file__).parent / "clients/drag_and_drop_demo.py"
 
 
 @pytest.mark.xdg(
@@ -78,7 +60,7 @@ class TestDragAndDrop:
             ("python3", APP_PATH, "--source", "text", "--target", "text", "--expect", "text"),
         ],
     )
-    async def test_source_and_dest_match(self, modern_server, app, tmp_path) -> None:
+    async def test_source_and_dest_match(self, modern_server, app, request) -> None:
         extensions = VirtualPointer.required_extensions + ScreencopyTracker.required_extensions
         server_instance = DisplayServer(modern_server, add_extensions=extensions)
         program = server_instance.program(apps.App(app))
@@ -86,22 +68,18 @@ class TestDragAndDrop:
         robot_test_case = dedent(
             """\
             Source and Destination Match
-                ${pos}=    Move Pointer To ${SRC_TEMPLATE}
+                ${pos}=    Move Pointer To ${template_path}/drag_and_drop_src.png
                 Press LEFT Button
-                Walk Pointer From ${pos} To ${DST_TEMPLATE}
+                Walk Pointer From ${pos} To ${template_path}/drag_and_drop_dst.png
                 Release LEFT Button
         """
         )
 
-        with tempfile.NamedTemporaryFile(mode="w+", suffix=".robot", buffering=1) as robot_file:
-            robot_file.write(
-                ROBOT_TEMPLATE.format(settings=ROBOT_SETTINGS, variables=ROBOT_VARIABLES, test_case=robot_test_case)
-            )
-            robot = server_instance.program(apps.App(("robot", "-d", tmp_path, robot_file.name)))
+        robot = server_instance.program(WaylandRobot(request, robot_test_case))
 
-            async with server_instance, program, robot:
-                await robot.wait(60)
-                await program.wait()
+        async with server_instance, program, robot:
+            await robot.wait(60)
+            await program.wait()
 
     @pytest.mark.parametrize(
         "app",
@@ -112,7 +90,7 @@ class TestDragAndDrop:
             ("python3", "-u", APP_PATH, "--source", "text", "--target", "pixbuf", "--expect", "pixbuf"),
         ],
     )
-    async def test_source_and_dest_mismatch(self, modern_server, app, tmp_path) -> None:
+    async def test_source_and_dest_mismatch(self, modern_server, app, request) -> None:
         extensions = VirtualPointer.required_extensions + ScreencopyTracker.required_extensions
         server_instance = DisplayServer(modern_server, add_extensions=extensions)
         program = server_instance.program(apps.App(app))
@@ -120,22 +98,18 @@ class TestDragAndDrop:
         robot_test_case = dedent(
             """\
             Source and Destination Mismatch
-                ${pos}=    Move Pointer To ${SRC_TEMPLATE}
+                ${pos}=    Move Pointer To ${template_path}/drag_and_drop_src.png
                 Press LEFT Button
-                ${pos}=    Walk Pointer From ${pos} To ${DST_TEMPLATE}
+                ${pos}=    Walk Pointer From ${pos} To ${template_path}/drag_and_drop_dst.png
                 Release LEFT Button
-                Walk Pointer From ${pos} To ${END_TEMPLATE}
+                Walk Pointer From ${pos} To ${template_path}/drag_and_drop_end.png
         """
         )
 
-        with tempfile.NamedTemporaryFile(mode="w+", suffix=".robot", buffering=1) as robot_file:
-            robot_file.write(
-                ROBOT_TEMPLATE.format(settings=ROBOT_SETTINGS, variables=ROBOT_VARIABLES, test_case=robot_test_case)
-            )
-            robot = server_instance.program(apps.App(("robot", "-d", tmp_path, robot_file.name)))
+        robot = server_instance.program(WaylandRobot(request, robot_test_case))
 
-            async with server_instance, program, robot:
-                await robot.wait(60)
-                assert program.is_running()
-                await program.kill()
-            assert "drag-begin\ndrag-failed\nenter-notify-event: dropbox" in program.output
+        async with server_instance, program, robot:
+            await robot.wait(60)
+            assert program.is_running()
+            await program.kill()
+        assert "drag-begin\ndrag-failed\nenter-notify-event: dropbox" in program.output
