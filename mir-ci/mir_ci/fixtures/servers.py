b9f1344722e1d366607cfa90f2561e1325f9722b
Michał Sawicz <michal@sawicz.net>
2024-03-25 11:56:02 +0100

    servers: add support for per-server marks

--- mir-ci/mir_ci/fixtures/servers.py
+++ mir-ci/mir_ci/fixtures/servers.py
@@ -1,6 +1,6 @@
 from collections.abc import Generator
 from enum import Flag, auto
-from typing import Callable, Sequence
+from typing import Any, Callable, Mapping, Sequence
 
 from ..program import app
 from ..program.display_server import DisplayServer
@@ -51,7 +51,7 @@ def server_params(caps: ServerCap = ServerCap.NONE) -> Generator[Server, None, N
     return (v for k, v in _SERVERS if caps in k)
 
 
-def servers(caps: ServerCap = ServerCap.NONE) -> Sequence[app.App]:
+def servers(caps: ServerCap = ServerCap.NONE, kwargss: Mapping[Server, Any] = {}) -> Sequence[app.App]:
     """
     Use this in test parametrization to select servers with the necessary capabilities for the
     test in question.
@@ -59,10 +59,21 @@ def servers(caps: ServerCap = ServerCap.NONE) -> Sequence[app.App]:
     >>> @pytest.mark.parametrize("server", servers(ServerCap.FLOATING_WINDOWS))
     >>> def test_func(server):
             pass
+
+    >>> @pytest.mark.parametrize(
+            "server",
+            servers(ServerCap.FLOATING_WINDOWS, {
+                ubuntu_frame: {
+                    marks=(pytest.mark.skip,)
+                }
+            })
+        )
+    >>> def test_func(server):
+            pass
     """
     # This tuple has to be realized here, otherwise class-wide parametrization
     # will exhaust the generator on the first test function.
-    return tuple(p() for p in server_params(caps))
+    return tuple(p(**kwargss.get(p, {})) for p in server_params(caps))
 
 
 @server(ServerCap.ALL ^ (ServerCap.FLOATING_WINDOWS | ServerCap.DISPLAY_CONFIG))
@@ -74,37 +85,35 @@ def ubuntu_frame():
     ServerCap.ALL
     ^ (ServerCap.FLOATING_WINDOWS | ServerCap.DRAG_AND_DROP | ServerCap.DISPLAY_CONFIG | ServerCap.INPUT_METHOD)
 )
-def mir_kiosk():
-    return snap("mir-kiosk", id="mir_kiosk")
+def mir_kiosk(*args, id="mir_kiosk", **kwargs):
+    return snap("mir-kiosk", *args, id=id, **kwargs)
 
 
 @server(ServerCap.ALL ^ ServerCap.DISPLAY_CONFIG)
-def confined_shell():
-    return snap("confined-shell", channel="edge", id="confined_shell")
+def confined_shell(*args, channel="edge", id="confined_shell", **kwargs):
+    return snap("confined-shell", *args, channel=channel, id=id, **kwargs)
 
 
 @server(ServerCap.ALL ^ ServerCap.DISPLAY_CONFIG)
-def mir_test_tools():
-    return snap("mir-test-tools", channel="22/beta", cmd=("mir-test-tools.demo-server",), id="mir_test_tools")
+def mir_test_tools(*args, channel="22/beta", cmd=("mir-test-tools.demo-server",), id="mir_test_tools", **kwargs):
+    return snap("mir-test-tools", channel=channel, cmd=(*cmd, *args), id=id, **kwargs)
 
 
 @server
-def mir_demo_server():
-    return deb("mir_demo_server", debs=("mir-test-tools", "mir-graphics-drivers-desktop"), id="mir_demo_server")
+def mir_demo_server(*args, debs=("mir-test-tools", "mir-graphics-drivers-desktop"), id="mir_demo_server", **kwargs):
+    return deb("mir_demo_server", *args, debs=debs, id=id, **kwargs)
 
 
 @server
-def miriway():
-    return snap("miriway", channel="stable", classic=True)
+def miriway(*args, channel="stable", classic=True, **kwargs):
+    return snap("miriway", *args, channel=channel, classic=classic, **kwargs)
 
 
 @server(ServerCap.ALL ^ (ServerCap.DISPLAY_CONFIG | ServerCap.SCREENCOPY | ServerCap.INPUT_METHOD))
-def gnome_shell():
-    return deb(
-        "gnome-shell",
-        "--wayland",
-        "--no-x11",
-        "--wayland-display",
-        DisplayServer.get_wayland_display(),
-        id="gnome_shell",
-    )
+def gnome_shell(
+    *args,
+    cmd=("gnome-shell", "--wayland", "--no-x11", "--wayland-display", DisplayServer.get_wayland_display()),
+    id="gnome_shell",
+    **kwargs,
+):
+    return deb("gnome-shell", *args, cmd=(*cmd, *args), id=id, **kwargs)
