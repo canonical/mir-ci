ecf73cf46efd138205de60865109cfa14b5b2abe
Harlen Batagelo <hbatagelo@gmail.com>
2024-05-23 17:42:41 -0300

    Remove unused protocol extension

--- mir-ci/mir_ci/wayland/protocols/__init__.py
+++ mir-ci/mir_ci/wayland/protocols/__init__.py
@@ -1,5 +1,4 @@
 from .wayland import *  # noqa: F401,F403
-from .virtual_keyboard_unstable_v1 import *  # noqa: F401,F403
 from .wlr_screencopy_unstable_v1 import *  # noqa: F401,F403
 from .wlr_virtual_pointer_unstable_v1 import *  # noqa: F401,F403
 from .xdg_output_unstable_v1 import *  # noqa: F401,F403
