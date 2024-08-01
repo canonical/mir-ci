085c1544b2037fb6099e323b03ff53255eb3000a
Tarek Ismail <tarek.ismail@canonical.com>
2024-08-01 13:15:35 +0300

    Use `DisplayServerStaticFile` to test different fractional scales

--- mir-ci/mir_ci/tests/test_fractional_scale_v1.py
+++ mir-ci/mir_ci/tests/test_fractional_scale_v1.py
@@ -7,7 +7,9 @@ import pytest
 from mir_ci import VARIANT
 from mir_ci.fixtures.servers import ServerCap, servers
 from mir_ci.program.app import App, AppType
-from mir_ci.program.display_server import DisplayServer
+# from mir_ci.program.display_server import DisplayServer
+
+from display_server_static_file import DisplayServerStaticFile
 
 TESTS_PATH = Path(__file__).parent
 APP_PATH = "gtk4-demo"
@@ -33,6 +35,29 @@ def collect_assets(platform: str, resources: Collection[str], suite: str, varian
         if p.is_file()
     }
 
+def get_display_config(scale: float) -> dict:
+    return {
+        "layouts": {
+            "default": {
+                "cards": [
+                    {
+                        "card-id": 0,
+                        "unknown-1": {
+                            "state": "enabled",
+                            "mode": "1280x1024@60",
+                            "position": [
+                                0,
+                                0
+                            ],
+                            "orientation": "normal",
+                            "scale": scale,
+                            "group": 0
+                        }
+                    }
+                ]
+            }
+        }
+    }
 
 @pytest.mark.xdg(
     XDG_CONFIG_HOME={
@@ -52,6 +77,7 @@ def collect_assets(platform: str, resources: Collection[str], suite: str, varian
 )
 @pytest.mark.env(GSETTINGS_BACKEND="keyfile")
 @pytest.mark.parametrize("server", servers(ServerCap.FLOATING_WINDOWS | ServerCap.DISPLAY_CONFIG))
+@pytest.mark.parametrize("scale", [1.0, 1.25, 1.5, 1.75, 2.0])
 @pytest.mark.deps(
     debs=(
         "gtk-4-examples",
@@ -64,13 +90,20 @@ def collect_assets(platform: str, resources: Collection[str], suite: str, varian
     ),
 )
 class TestFractionalScaleV1:
-    async def test_fractional_scale_v1(self, robot_log, server, tmp_path) -> None:
-        extensions = ('all',)
-        server_instance = DisplayServer(server, add_extensions=extensions)
+    async def test_fractional_scale_v1(self, robot_log, server, scale, tmp_path) -> None:
+        extensions = ('all',)  # TODO no need to enable all extension
+        display_config = get_display_config(scale)
+
+        server_instance = DisplayServerStaticFile(server, add_extensions=extensions)
+        server_instance.write_config(display_config)
+
         assets = collect_assets("wayland", ["kvm"], "fractional_scale_v1")
 
-        async with server_instance, server_instance.program(App(APP_PATH, AppType.deb)):
+        async with server_instance, server_instance.server.program(App(APP_PATH, AppType.deb)):
             tuple((tmp_path / k).symlink_to(v) for k, v in assets.items())
-            robot = server_instance.program(App(("robot", "-d", tmp_path, "--log", robot_log, tmp_path)))
+
+            robot = server_instance.server.program(
+                App(("robot", "-d", tmp_path, "--log", robot_log, tmp_path)))
+
             async with robot:
                 await robot.wait(120)
