e41c3f6eba43a52e7d8238a3720b46684fad26ec
Michał Sawicz <michal@sawicz.net>
2026-01-23 15:51:19 +0100

    osk: get output resolution from the server
    Rather than expect it in the environment.

--- mir-ci/mir_ci/wayland/output_watcher.py
+++ mir-ci/mir_ci/wayland/output_watcher.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Callable, List, Optional
 
 from .protocols import WlOutput
@@ -17,12 +18,18 @@ class OutputWatcher(WaylandClient):
         super().__init__(display_name)
         self.wl_outputs: List[WlOutputProxy] = []
         self.callbacks = {"geometry": on_geometry, "mode": on_mode, "scale": on_scale, "name": on_name}
+        self.mode: Optional[tuple[int, int, int]] = None
+        self.done = False
 
     def registry_global(self, registry, id_num: int, iface_name: str, version: int) -> None:
         if iface_name == WlOutput.name:
             self.wl_outputs.append(registry.bind(id_num, WlOutput, min(WlOutput.version, version)))
             for key in self.callbacks:
+                if key == "mode":
+                    continue
                 self.wl_outputs[-1].dispatcher[key] = self.callbacks[key]
+            self.wl_outputs[-1].dispatcher["mode"] = self.on_mode
+            self.wl_outputs[-1].dispatcher["done"] = self.on_done
             self.display.roundtrip()
 
     def connected(self) -> None:
@@ -30,3 +37,16 @@ class OutputWatcher(WaylandClient):
 
     def disconnected(self) -> None:
         pass
+
+    def on_mode(self, wl_output: WlOutput, flags: int, width: int, height: int, refresh: int) -> None:
+        if self.callbacks["mode"] is not None:
+            self.callbacks["mode"](wl_output, flags, width, height, refresh)  # type: ignore
+        if self.mode is None:
+            self.mode = (width, height, refresh)
+
+    def on_done(self, wl_output: WlOutput) -> None:
+        self.done = True
+
+    async def wait_done(self) -> None:
+        while self.done is False:
+            await asyncio.sleep(0.1)
